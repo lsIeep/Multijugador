303115b748f3907409e98856d307b3476051246c
lsIeep <joseluis.marcelo27@gmail.com>
2025-02-25 09:24:16 +0100

    3

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using ED.SC;
-using Mono.Cecil;
+//using Mono.Cecil;
 using Unity.Mathematics;
 using Unity.Netcode;
 using UnityEngine;
@@ -10,7 +10,7 @@ using Random = UnityEngine.Random;
 public class AvatarController : NetworkBehaviour {
     [SerializeField] private float movementSpeed = 3.0f;
     [SerializeField] private GameObject doorPrefab;
-    private readonly List<Color> m_Colors = new List<Color>() { Color.blue , Color.green, Color.red};
+    private readonly List<Color> m_Colors = new List<Color>() { Color.blue , Color.green, Color.red, Color.gray};
     private SpriteRenderer m_Renderer;
     private static NetworkVariable<Vector3> m_Position = new NetworkVariable<Vector3>(Vector3.zero, 
         NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
@@ -43,30 +43,33 @@ public class AvatarController : NetworkBehaviour {
 
         // sets color to network variable value
         if (m_Renderer.color != m_Color.Value) m_Renderer.color = m_Color.Value;
-        
+
         if (!IsOwner) return;
 
         /*if (Input.GetKeyDown(KeyCode.T))
         {
             randomNumber.Value = Random.Range(0, 100);
         }*/
-        
+
         // movement
         if (Input.GetKey(KeyCode.A)) LerpPosition(Vector3.left, movementSpeed);
         if (Input.GetKey(KeyCode.D)) LerpPosition(Vector3.right, movementSpeed);
         if (Input.GetKey(KeyCode.W)) LerpPosition(Vector3.up, movementSpeed);
         if (Input.GetKey(KeyCode.S)) LerpPosition(Vector3.down, movementSpeed);
-        
+
         // color change
         // if you need to change something not in transform, it's needed RPC calling
         // RPC calling executes the method in several clients (specified by RpcTarget)
         // if (Input.GetKeyDown(KeyCode.RightControl)) m_Color.Value = m_Colors[2];
         if (Input.GetKeyDown(KeyCode.RightControl)) SpawnDoorServerRpc();
 
-        if(Input.GetKeyDown(KeyCode.T)) TestClientRpc();
+        /*if (Input.GetKeyDown(KeyCode.T))
+        {
+            TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } });
+        }*/
     }
 
-    void LerpPosition(Vector3 offset, float speed) {
+        void LerpPosition(Vector3 offset, float speed) {
         Vector3 positionNow = transform.position;
         transform.position = Vector3.Lerp(positionNow, positionNow +  offset, Time.deltaTime * speed);
     }
@@ -81,14 +84,14 @@ public class AvatarController : NetworkBehaviour {
         door.GetComponent<NetworkObject>().Spawn();
     }
 
-    [Rpc(SendTo.Server)]
+    [ServerRpc]
     void TestServerRpc(ServerRpcParams serverRpcParams)
     {
         SmartConsole.Log("TestServerRpc " + OwnerClientId + "," + serverRpcParams.Receive.SenderClientId);
     }
 
     [ClientRpc]
-    private void TestClientRpc()
+    private void TestClientRpc(ClientRpcParams clientRpcParams)
     {
         SmartConsole.Log("TestClientRpc");
     }
